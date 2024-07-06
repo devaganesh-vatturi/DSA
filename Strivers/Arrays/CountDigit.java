deecbf0d7024698909e80bbb869e0393adf2b30f
devaganeshvatturi <devavatturi@gmail.com>
2024-07-06 12:05:50 +0530

    deva added folders

---
+++ Strivers/Arrays/CountDigit.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CountDigit {
+    public static void main(String args[])
+    {
+        Scanner S=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=S.nextInt();
+        // String str = "" + n;
+        // System.out.println(str.length());
+        int count=0;
+        while(n!=0)
+        {
+            n=n/10;
+            count++;
+        }
+        System.out.println("the count is:"+count);
+
+        
+
+    }
+}
