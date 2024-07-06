1bf2df47dc42253d243cc04ad1a93d716e3ed1a7
devaganeshvatturi <devavatturi@gmail.com>
2024-07-06 19:06:40 +0530

    deva added reverse number

--- Strivers/BasicPractice/ReverseNumber.java
+++ Strivers/BasicPractice/ReverseNumber.java
@@ -1,18 +1,28 @@
 import java.util.*;
 class ReverseNumber {
-    public static void main(String[] args) {
-        System.out.println("Enter a number");
-        Scanner S=new Scanner(System.in);
-        int n=S.nextInt();
-        int newnum=0;
-        while(n!=0)
+    static int reverse(int x) 
+    {
+       
+       long newnum=0;
+        while(x!=0)
         {
-            int rem=n%10;
-            newnum=newnum*10+rem;
-            n=n/10;
+            int rem=x%10;
+           newnum+=rem;
+           newnum*=10;
+            x=x/10;
 
         }
-        System.out.println(newnum);
+        newnum/=10;
+         if (newnum <= Integer.MIN_VALUE || newnum >= Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+
+        return((int)newnum);
+    }
+    public static void main(String[] args) {
+        int value=reverse(3456);
+        System.out.println(value);
+    }
 
     }
-}
