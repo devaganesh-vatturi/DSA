bb36f8d208577210bfe86eef1aba54b6e4ad7c51
devaganeshvatturi <devavatturi@gmail.com>
2025-01-03 12:42:03 +0530

    added ones

---
+++ Strivers/Arrays/ArrayOnes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> nums={1,0,1,1,0,1};
+     int c=0,max=0;
+        for(int i:nums)
+        {
+            if(i==1)
+              c+=1;
+            else
+            c=0; 
+            if(max<c)
+                max=c; 
+        }
+        cout<<max<<endl;
+    return 0;
+}
