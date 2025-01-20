ae33a1d57e3ebec185d361c3beb77680ddcb5336
devaganeshvatturi <devavatturi@gmail.com>
2025-01-20 22:17:21 +0530

    added maxsubarray

---
+++ Strivers/Arrays/ArrayMaxSubArray.cpp
@@ -0,0 +1,19 @@
+//return maximum sum of a sub array
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> nums={-1};
+    int max=INT_MIN,min=INT_MIN,sum=0;
+    for(int i=0;i<nums.size();i++)
+    {
+    sum+=nums[i];
+    if(sum>max)
+       max=sum;
+    if(sum<0)
+       sum=0;   
+    }
+    cout<<max<<endl;
+    cout<<min<<endl;
+    return 0;
+}
